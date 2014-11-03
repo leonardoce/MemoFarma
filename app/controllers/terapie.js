6a55c82f086d4eae832133ed1d8d36e654997317
Leonardo Cecchi <leonardoce@interfree.it>
2014-11-03 20:48:09 +0100

    lavoro

--- app/controllers/terapie.js
+++ app/controllers/terapie.js
@@ -1,26 +1,14 @@
 var datiTerapie = Alloy.Collections.terapie;
 var args = arguments[0] || {};
 
-function aggiungiTerapia() {
+function onAggiungiTerapia() {
 	alert("Ciao");
 }
 
-function setupActionBar() {
-	$.terapie.activity.onCreateOptionsMenu = function(e) {
-		var menu = e.menu;
-		var menuitem;
-
-		menuitem = menu.add({
-			title: "Aggiungi",
-			showAsAction: Ti.Android.SHOW_AS_ACTION_ALWAYS
-		});
-		menuitem.addEventListener("click", aggiungiTerapia);
-	}
-
-	$.terapie.removeEventListener("open", setupActionBar);
-}
-
-$.terapie.addEventListener("open", setupActionBar);
 $.terapie.open();
 
 datiTerapie.fetch();
+if (datiTerapie.length>0) 
+{
+	$.nessuna_presente.visible = false;
+}
