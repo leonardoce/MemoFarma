e8910cb1237460f50211f493d693caa3411c10eb
Leonardo Cecchi <leonardoce@interfree.it>
2014-11-04 22:39:01 +0100

    lavoro su tab principale

--- app/controllers/tabprincipale.js
+++ app/controllers/tabprincipale.js
@@ -5,4 +5,9 @@ function onTerapie()
 	Alloy.createController("terapie").getView().open();
 }
 
+function onAiuto()
+{
+	Alloy.createController("tutorial").getView().open();
+}
+
 $.tabprincipale.open();
