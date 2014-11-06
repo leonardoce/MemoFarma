82b5f47a27ae469091f9edb5dc9afc2d1f64d18c
Leonardo Cecchi <leonardoce@interfree.it>
2014-11-06 21:09:42 +0100

    poteva andare peggio

---
+++ app/controllers/dettagli_pressioni.js
@@ -0,0 +1,74 @@
+var args = arguments[0] || {};
+var StringUtils = require("StringUtils");
+
+function doSalva()
+{
+	var massima = $.tf_massima.value.trim();
+	var minima = $.tf_minima.value.trim();
+	var frequenza = $.tf_frequenza.value.trim();
+	var automisurazione = $.cb_automisurazione.value;
+	var rilevazione_data = $.pk_rilevazione_data.value;
+	var rilevazione_ora = $.pk_rilevazione_ora.value;
+
+	var rilevazione = new Date(rilevazione_data);
+	rilevazione.setHours(rilevazione.getHours());
+	rilevazione.setMinutes(rilevazione.getMinutes());
+
+	if (!StringUtils.stringIsInteger(massima))
+	{
+		alert("Inserire correttamente la pressione massima");
+	}
+
+	if (!StringUtils.stringIsNumber(minima))
+	{
+		alert("Inserire correttamente la pressione minima");
+	}
+
+	if (!StringUtils.stringIsNumber(frequenza))
+	{
+		frequenza = 0; // non rilevata
+	}
+
+	if (args.modello)
+	{
+		args.modello.set({
+			massima: massima,
+			minima: minima,
+			frequenza: frequenza,
+			automisurazione: automisurazione,
+			rilevazione: rilevazione.toIsoString()
+		});
+
+		args.save();
+	}
+
+	args.close();
+}
+
+function doCancella()
+{
+	if (args.modello)
+	{
+		args.modello.destroy();
+	}
+
+	$.dettagli_pressioni.close();
+}
+
+function doOpen()
+{
+	if (args.modello)
+	{
+		$.tf_massima.value = args.modello.get("massima");
+		$.tf_minima.value = args.modello.get("minima");
+		$.tf_frequenza.value = args.modello.get("frequenza");
+		$.cb_automisurazione.value = StringUtils.string2logic(args.modello.get("automisurazione"));
+
+		if (args.modello.get("rilevazione"))
+		{
+			$.pk_rilevazione_data = new Date(args.modello.get("rilevazione"));
+			$.pk_rilevazione_ora = new Date(args.modello.get("rilevazione"));
+		}
+	}
+}
+
