feb5e72c370d303567a33bbc5413fe397dccc87c
Leonardo Cecchi <leonardoce@interfree.it>
2014-12-08 22:01:13 +0000

    indentazione

--- app/models/terapie.js
+++ app/models/terapie.js
@@ -1,42 +1,42 @@
 exports.definition = {
-	config: {
-		columns: {
-		    "nome": "text",
-		    "ora": "text",
-		    "dose": "text",
-		    "terapia_id": "integer primary key autoincrement",
-		    "data_inizio": "text",
-		    "data_fine": "text"
-		},
-		adapter: {
-			type: "sql",
-			collection_name: "terapie",
-			idAttribute: "terapia_id"
-		}
+    config: {
+	columns: {
+	    "nome": "text",
+	    "ora": "text",
+	    "dose": "text",
+	    "terapia_id": "integer primary key autoincrement",
+	    "data_inizio": "text",
+	    "data_fine": "text"
 	},
-	extendModel: function(Model) {
-		_.extend(Model.prototype, {
-			toString: function()
-			{
-				var StringUtils = require("StringUtils");
-
-				return "<Terapie id=" + StringUtils.stosingle(this.get("terapia_id")) + 
-					" nome=" + StringUtils.stosingle(this.get("nome")) + 
-					" ora=" + StringUtils.stosingle(this.get("ora")) + 
-					" dose=" + StringUtils.stosingle(this.get("dose")) + 
-					"/>"
-			}
-
-			// extended functions and properties go here
-		});
-
-		return Model;
-	},
-	extendCollection: function(Collection) {
-		_.extend(Collection.prototype, {
-			// extended functions and properties go here
-		});
-
-		return Collection;
+	adapter: {
+	    type: "sql",
+	    collection_name: "terapie",
+	    idAttribute: "terapia_id"
 	}
+    },
+    extendModel: function(Model) {
+	_.extend(Model.prototype, {
+	    toString: function()
+	    {
+		var StringUtils = require("StringUtils");
+
+		return "<Terapie id=" + StringUtils.stosingle(this.get("terapia_id")) + 
+		    " nome=" + StringUtils.stosingle(this.get("nome")) + 
+		    " ora=" + StringUtils.stosingle(this.get("ora")) + 
+		    " dose=" + StringUtils.stosingle(this.get("dose")) + 
+		    "/>"
+	    }
+
+	    // extended functions and properties go here
+	});
+
+	return Model;
+    },
+    extendCollection: function(Collection) {
+	_.extend(Collection.prototype, {
+	    // extended functions and properties go here
+	});
+
+	return Collection;
+    }
 };
