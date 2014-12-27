2b892d38752fe79f04b63bb25de99363e00a5251
Leonardo Cecchi <leonardoce@interfree.it>
2014-12-27 23:15:36 +0100

    introdotta localizzazione e tutorial

--- app/controllers/tutorial.js
+++ app/controllers/tutorial.js
@@ -1,3 +1,9 @@
 var args = arguments[0] || {};
 
+var currentLocale = Titanium.Locale.currentLanguage;
+if(currentLocale!=="it" && currentLocale!=="en") 
+{
+	currentLocale = "en";
+}
+$.paginaweb.url = "/tutorial/tutorial-" + currentLocale + ".html";
 $.tutorial.open();
