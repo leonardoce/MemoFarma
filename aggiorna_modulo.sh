1b5542d41e542fe52dedff77ffb4f060dc494b7d
Leonardo Cecchi <leonardoce@interfree.it>
2015-01-03 16:34:33 +0100

    script per aggiornare il modulo nativo

---
+++ aggiorna_modulo.sh
@@ -0,0 +1,4 @@
+# Copia il nuovo modulo compilato
+rm -r build/ modules/android/it.interfree.leonardoce.bootreceiver/
+cp ../leo_bootreceiver/android/dist/it.interfree.leonardoce.bootreceiver-android-1.0.0.zip .
+
