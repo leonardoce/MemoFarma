cad38a7cc6f1487787f8c0c352e1ccc805409c1d
Leonardo Cecchi <leonardoce@interfree.it>
2015-01-11 10:31:11 +0100

    aggiunta clausola all'inizio dei fogli di stile

--- app/alloy.js
+++ app/alloy.js
@@ -1,3 +1,18 @@
+// This file is part of MemoFarma.
+//
+// MemoFarma is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// MemoFarma is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with MemoFarma.  If not, see <http://www.gnu.org/licenses/>.
+
 // The contents of this file will be executed before any of
 // your view controllers are ever executed, including the index.
 // You have access to all functionality on the `Alloy` namespace.
@@ -47,7 +62,7 @@ else if(tipologia=="controllo_al_boot")
 
 	// Oltre a questo alcuni telefoni (vedi Samsung Galaxy Grand) perdono
 	// tutti gli allarmi al boot. Son misteri.
-	GestoreAllarmi.attivaGestioneAllarmi();	
+	GestoreAllarmi.attivaGestioneAllarmi();
 
 	// Ho finito i miei controlli
 	var activity = Titanium.Android.currentActivity;
